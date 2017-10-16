608a4dbe1c0daf2d43df1e420b894924edc57257
isak-g <isak16@ru.is>
2017-10-16 15:02:21 +0000

    Add two numbers method

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -7,10 +7,24 @@ public class Calculator{
 			return 0;
 		}
 		else{
+			if (text.contains(",")) {
+				String numbers[] = text.split(","); 
+				int total = 0;
+				for(String number : numbers)
+				{
+					total += toInt(number);
+				}
+				return total;
+			}
+
+
 			return 1;
 		}
 		
-		
 	}
+
+	private static int toInt(String number){
+			return Integer.parseInt(number);
+		}
 
 }
